4bb275550feb05befd64a0bbac5b3f15ee5efde3
stacktea <hariomtiwari0241@gmail.com>
2022-06-05 11:58:21 +0530

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,9 +2,11 @@ const express = require('express')
 const userRouter = express.Router()
 const { registerUser, loginUser, getme } = require('../controllers/userController')
 
+const { protect } = require('../middleware/authMiddleware')
+
 
 userRouter.route('/register').post(registerUser)
 userRouter.route('/login').post(loginUser)
-userRouter.get('/me', getme)
+userRouter.get('/me', protect, getme)
 
 module.exports = userRouter
