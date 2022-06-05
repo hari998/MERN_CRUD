d58e7a54b7a0aa5e99bae2375da0bcfb8e3eed9f
stacktea <hariomtiwari0241@gmail.com>
2022-06-05 11:58:34 +0530

    Update quoteschema.js

--- backend/models/quoteschema.js
+++ backend/models/quoteschema.js
@@ -1,9 +1,20 @@
 const mongoose = require('mongoose')
 
+/* 
+pt1 - this will allow us, make user associated with the quote, means which user has written which quotes
+pt2 - this will set the mongodb ObjectId as its type
+pt3 - this 'ref' means, to which model the ObjectId pertains to
+*/
+
 const quoteSchema = mongoose.Schema({
+    user: {                                     //pt1
+        type: mongoose.Schema.Types.ObjectId,   //pt2
+        required: true,
+        ref: 'User'                             //pt3
+    },
     name: {
         type: String,
-        required: [true, 'please add your name']
+        required: [true, 'please add a name']
         // error status 500 is sent if validation error from mongoose
     },
     quote: {
@@ -14,7 +25,6 @@ const quoteSchema = mongoose.Schema({
     {
         timestamps: true,
     }
-
 )
 
 module.exports = mongoose.model('Quotes', quoteSchema)
